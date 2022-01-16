4657b2569c954c0aef50ea061ee7921fc20da6a4
Ayushi Tiwari <tayushi1610@gmail.com>
2022-01-16 23:22:45 +0530

    Update and rename First Unique Character in a String to First-Unique-Character-in-a-String.java

--- First-Unique-Character-in-a-String.java
+++ First-Unique-Character-in-a-String.java
@@ -1,8 +1,8 @@
-Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+//Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
  
 
-Example 1:
+/*Example 1:
 
 Input: s = "leetcode"
 Output: 0
@@ -10,7 +10,7 @@ Example 2:
 
 Input: s = "loveleetcode"
 Output: 2
-
+*/
 
 class Solution {
     public int firstUniqChar(String s) {
