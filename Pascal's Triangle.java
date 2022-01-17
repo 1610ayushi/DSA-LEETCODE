206bc8f152490331ef641ccad65ee6e42f8c13f3
Ayushi Tiwari <tayushi1610@gmail.com>
2022-01-17 23:20:12 +0530

    Update and rename Pascal's Triangle to Pascal's Triangle.java

--- Pascal's Triangle.java
+++ Pascal's Triangle.java
@@ -1,9 +1,9 @@
-Given an integer numRows, return the first numRows of Pascal's triangle.  In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+/*Given an integer numRows, return the first numRows of Pascal's triangle.  In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
 Example 1:
 
 Input: numRows = 5
-Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]*/
 
 class Solution {
     public List<List<Integer>> generate(int numRows) {
