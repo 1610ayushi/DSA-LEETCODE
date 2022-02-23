2ef9fc3dfa73ec66a8dd3c9258d79e386b6a9463
Ayushi Tiwari <tayushi1610@gmail.com>
2022-02-23 21:06:39 +0530

    Create Clone Graph.java

---
+++ Clone Graph.java
@@ -0,0 +1,76 @@
+/*
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+ 
+
+Example 1:
+
+
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+          if (node == null) {
+      return null;
+    }
+    Map<Node, Node> map = new HashMap<>();
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(node);
+    map.put(node, new Node(node.val));
+    while (!queue.isEmpty()) {
+      Node removed = queue.remove();
+      for (Node neighbor : removed.neighbors) {
+        if (!map.containsKey(neighbor)) {
+          map.put(neighbor, new Node(neighbor.val));
+          queue.add(neighbor);
+        }
+        map.get(removed).neighbors.add(map.get(neighbor));
+      }
+    }
+    return map.get(node);
+    }
+}
