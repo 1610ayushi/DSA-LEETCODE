3dafd102b870b8fc8e0865a83823acf11e77ac74
Ayushi Tiwari <tayushi1610@gmail.com>
2022-03-24 20:03:53 +0530

    Create boats to save people.java

---
+++ boats to save people.java
@@ -0,0 +1,20 @@
+/*
+You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
+*/class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int ans = 0;
+
+        while (i <= j) {
+            ans++;
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+
+        return ans;
+    }
+}
