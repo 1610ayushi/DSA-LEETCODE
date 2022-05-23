e9f7b1658a2e1197aba0aa303bb7ca68073485f1
Ayushi Tiwari <tayushi1610@gmail.com>
2022-05-23 21:34:35 +0530

    Create 474. Ones and Zeroes.java

---
+++ 474. Ones and Zeroes.java
@@ -0,0 +1,52 @@
+/*
+You are given an array of binary strings strs and two integers m and n.
+
+Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
+
+A set x is a subset of a set y if all elements of x are also elements of y.
+
+ 
+
+Example 1:
+
+Input: strs = ["10","0001","111001","1","0"], m = 5, n = 3
+Output: 4
+Explanation: The largest subset with at most 5 0's and 3 1's is {"10", "0001", "1", "0"}, so the answer is 4.
+Other valid but smaller subsets include {"0001", "1"} and {"10", "1", "0"}.
+{"111001"} is an invalid subset because it contains 4 1's, greater than the maximum of 3.
+Example 2:
+
+Input: strs = ["10","0","1"], m = 1, n = 1
+Output: 2
+Explanation: The largest subset is {"0", "1"}, so the answer is 2.
+ 
+
+Constraints:
+
+1 <= strs.length <= 600
+1 <= strs[i].length <= 100
+strs[i] consists only of digits '0' and '1'.
+1 <= m, n <= 100
+*/
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] arr = new int[m+1][n+1];
+        
+        for(String str : strs){
+            int c0 = 0;
+            int c1 = 0;
+            
+            for(char c : str.toCharArray()){
+                if(c == '0') c0++;
+                else c1++;
+            }
+            
+            for(int i=m;i>=c0;i--){
+                for(int j=n;j>=c1;j--){
+                    arr[i][j] = Math.max(arr[i][j],arr[i-c0][j-c1] + 1);
+                }
+            }
+        }
+        return arr[m][n];
+    }
+}
