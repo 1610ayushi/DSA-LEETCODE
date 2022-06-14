15e06cabedafd659fc69359f32502ebe1731bc9a
Ayushi Tiwari <tayushi1610@gmail.com>
2022-06-14 15:36:31 +0530

    Create 583. Delete Operation for Two Strings.java

---
+++ 583. Delete Operation for Two Strings.java
@@ -0,0 +1,41 @@
+
+/*
+Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
+
+In one step, you can delete exactly one character in either string.
+
+ 
+
+Example 1:
+
+
+Input: word1 = "sea", word2 = "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+Example 2:
+
+Input: word1 = "leetcode", word2 = "etco"
+Output: 4
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 500
+word1 and word2 consist of only lowercase English letters.*/
+public class Solution {
+    public int minDistance(String s1, String s2) {
+        int[][] memo = new int[s1.length() + 1][s2.length() + 1];
+        return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length(), memo);
+    }
+    public int lcs(String s1, String s2, int m, int n, int[][] memo) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (memo[m][n] > 0)
+            return memo[m][n];
+        if (s1.charAt(m - 1) == s2.charAt(n - 1))
+            memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+        else
+            memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
+        return memo[m][n];
+    }
+}
