1c12187c277a71c969d1381c15712b49d5b8427d
Ayushi Tiwari <tayushi1610@gmail.com>
2022-10-09 21:02:12 +0530

    Create 653. Two Sum IV - Input is a BST.java

---
+++ 653. Two Sum IV - Input is a BST.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+
+        HashSet<Integer>set=new HashSet<>();
+        boolean ans= func(root, k, set);
+        return ans;
+    }
+    public boolean func(TreeNode root, int k, HashSet<Integer>set){
+        if(root==null)return false;
+        if(func(root.left,k,set)==true){
+            return true;
+        }
+        if(set.contains(k-root.val)){
+            return true;
+        }
+        set.add(root.val);
+        return func(root.right, k,set);
+    }
+}
